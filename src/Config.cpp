3c22337e4c4ccf88985c06dddaf4ddac760391ab
提米吖 <2847696890@qq.com>
2024-08-20 01:18:15 +0800

    feat(config): update PluginConfig and remove unnecessary includes

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,7 +1,12 @@
 #include "Config.h"
 
 
-// namespace TSEssential::Config {
-// static 
-
-// }
+namespace TSEssential::Config {
+static PluginConfig *instance;
+PluginConfig *PluginConfig::GetInstance() {
+    return instance;
+}
+PluginConfig::PluginConfig() {
+  
+}
+}
