b3b79104cd2bb850704dcdd2476abfe3b6f36e0d
提米吖 <2847696890@qq.com>
2024-08-19 23:05:13 +0800

    ProcessException add SEH_EXCEPTION

--- src/Exception.cc
+++ src/Exception.cc
@@ -1,6 +1,9 @@
 #include "./Exception.h"
 #include "./TSEssential.h"
+
 #include <string>
+
+#include <ll/api/utils/ErrorUtils.h>
 
 
 namespace LL3Mine2_Class::Exception {
@@ -10,17 +13,20 @@ void ProcessException(std::exception_ptr ptr) {
     if (ptr) {
         try {
             std::rethrow_exception(ptr);
-        } catch (const std::exception& exc) {
-            LOGGER.error("Exception Message:");
+        } catch (const ll::error_utils::seh_exception& exc) {
+            LOGGER.error("Exception(SEH_EXCEPTION) Message:");
+            LOGGER.error(exc.what());
+        }  catch (const std::exception& exc) {
+            LOGGER.error("Exception(STD) Message:");
             LOGGER.error(exc.what());
         } catch (const std::string &str) {
             LOGGER.error("Exception(string) Content:");
             LOGGER.error(str);
-        } catch (const int &v) {
-            LOGGER.error("Exception(int) Content:");
-            LOGGER.error("{0}", v);
+        // } catch (const int &v) {
+        //     LOGGER.error("Exception(int) Content:");
+        //     LOGGER.error("{0}", v);
         } catch (...) {
-          LOGGER.error("Abnormal info is not an abnormal type!");
+            LOGGER.error("Abnormal info is not an abnormal type!");
         }
     } else {
         LOGGER.error("Unable to resolve exception!");
