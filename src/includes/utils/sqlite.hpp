163633f0cb7feb68f2b8385f6f78cecdacc9bf93
Singl-Daemon <SingleIsland@outlook.com>
2024-08-21 02:10:55 +0800

    feat: add SQLite API(shit

--- src/includes/utils/sqlite.hpp
+++ src/includes/utils/sqlite.hpp
@@ -1,19 +1,29 @@
 // 答应群里的要求,LL3 TMET使用数据库来储存数据
 
-#include <sqlite3.h>
 #include <string>
+#include <map>
+#include <vector>
+#include "sqlite3.h"
 
-class SQLite {
+using ResultMap = std::vector<std::map<std::string, std::string>>;
+
+struct SQLite {
 public:
-    // maybe nullptr
-    static SQLite* Get(std::string& name);
-    static void    CloseAllDB();
-    SQLite(std::string filename);
+    SQLite(std::string const& db_name_);
     ~SQLite();
-
+    SQLite(sqlite3* db) : db(db) {}
+    SQLite* Get(std::string& name);
+    void Open();
     void Close();
+    bool ExecSQLCommand(std::string const& updateSql);
+    bool CreateTable(const std::string& tableName, const std::string& columns);
+    bool Insert(const std::string& tableName, const std::string& values);
+    bool Update(const std::string& tableName, const std::string& setColumns, const std::string& conditions);
+    bool Delete(const std::string& tableName, const std::string& conditions);
 
+    ResultMap Query(std::string const& querySql);
 private:
-    std::string name;
-    sqlite3* db;
+    std::string db_name;
+    sqlite3 *db;
+    bool is_closed {true};
 };
---
+++ src/utils/sqlite.cpp
@@ -0,0 +1,159 @@
+#include "utils/sqlite.hpp"
+
+#include "TSEssential.h"
+
+/**
+ * @brief SQLite类的构造函数
+ * @param db_name_ 数据库名称
+ */
+SQLite::SQLite(std::string const& db_name_) : db_name(db_name_) {}
+
+/**
+ * @brief SQLite类的Get函数
+ */
+SQLite* Get(std::string& name) {
+    sqlite3* db;
+    int      rc = sqlite3_open(name.c_str(), &db);
+    if (rc) {
+        LOGGER.error("Can't open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return nullptr;
+    }
+    return new SQLite(db);
+}
+
+/**
+ * @brief 打开数据库
+ */
+void SQLite::Open() {
+    int rc;
+    /* Open database */
+    rc = sqlite3_open(db_name.data(), &db);
+    if (rc) {
+        LOGGER.error("Can't open database: %s\n", sqlite3_errmsg(db));
+    } else {
+        is_closed = false;
+        LOGGER.info("Opened database successfully");
+    }
+}
+
+/**
+ * @brief 关闭数据库
+ */
+void SQLite::Close() {
+    LOGGER.info("Closing database...");
+    sqlite3_close(db);
+    is_closed = true;
+}
+
+/**
+ * @brief 执行SQL命令
+ * @param updateSql SQL命令
+ * @return 执行结果
+ */
+bool SQLite::ExecSQLCommand(std::string const& updateSql) {
+    char* zErrMsg = 0;
+    int   rc      = sqlite3_exec(db, updateSql.data(), nullptr, 0, &zErrMsg);
+    if (rc != SQLITE_OK) {
+        LOGGER.error("SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+        return false;
+    } else {
+        LOGGER.info("Operation done successfully\n");
+        return true;
+    }
+}
+
+/**
+ * @brief 创建表
+ * @param tableName 表名
+ * @param columns 列名和数据类型
+ * @return 执行结果
+ */
+bool SQLite::CreateTable(const std::string& tableName, const std::string& columns) {
+    std::string sql = "CREATE TABLE " + tableName + " (" + columns + ")";
+    return ExecSQLCommand(sql);
+}
+
+/**
+ * @brief 插入数据
+ * @param tableName 表名
+ * @param values 数据值
+ * @return 执行结果
+ */
+bool SQLite::Insert(const std::string& tableName, const std::string& values) {
+    std::string sql = "INSERT INTO " + tableName + " (" + values + ")";
+    return ExecSQLCommand(sql);
+}
+
+/**
+ * @brief 更新数据
+ * @param tableName 表名
+ * @param setColumns 要更新的列名和数据值
+ * @param conditions 更新条件
+ * @return 执行结果
+ */
+bool SQLite::Update(const std::string& tableName, const std::string& setColumns, const std::string& conditions) {
+    std::string sql = "UPDATE " + tableName + " SET " + setColumns + " WHERE " + conditions;
+    return ExecSQLCommand(sql);
+}
+
+/**
+ * @brief 删除数据
+ * @param tableName 表名
+ * @param conditions 删除条件
+ * @return 执行结果
+ */
+bool SQLite::Delete(const std::string& tableName, const std::string& conditions) {
+    std::string sql = "DELETE FROM " + tableName + " WHERE " + conditions;
+    return ExecSQLCommand(sql);
+}
+
+/**
+ * @brief 查询数据
+ * @param querySql 查询SQL语句
+ * @return 查询结果
+ */
+ResultMap SQLite::Query(std::string const& querySql) {
+    ResultMap results;
+    char*     zErrMsg = 0;
+    /* Execute SQL statement */
+    int rc = sqlite3_exec(
+        db,
+        querySql.data(),
+        [](void* data, int argc, char** argv, char** azColName) {
+            int                                i;
+            auto&                              results_ = *((ResultMap*)(data));
+            std::map<std::string, std::string> result;
+            for (i = 0; i < argc; i++) {
+                if (argv[i]) {
+                    result[azColName[i]] = argv[i];
+                } else {
+                    result[azColName[i]] = "NULL";
+                }
+            }
+            results_.emplace_back(std::move(result));
+            return 0;
+        },
+        (void*)&results,
+        &zErrMsg
+    );
+
+    if (rc != SQLITE_OK) {
+        LOGGER.error("SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        LOGGER.info("Operation done successfully\n");
+    }
+    return results;
+}
+
+/**
+ * @brief 析构函数
+ */
+SQLite::~SQLite() {
+    if (!is_closed) {
+        LOGGER.info("Closing database...");
+        sqlite3_close(db);
+    }
+}
